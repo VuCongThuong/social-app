6e774fdf755d9711a80510c2a58adfb7b1898aad
Vu Cong Thuong <thuong@redweb.dk>
2023-05-17 10:55:01 +0700

    Social App

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+// const multer = require("multer");
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+// const postRoute = require("./routes/posts");
+// const router = express.Router();
+// const path = require("path");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }).catch(function (err) {
+    console.log('DB connection failed: ', err);
+    process.exit(1);
+});
+
+//middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+
+app.listen(8800, () => {
+    console.log('Backend server is running!');
+});
---
+++ api/routes/auth.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const User = require('../models/Users');
+const bcrypt = require('bcrypt');
+const { route } = require('./users');
+
+// REGISTER
+router.post('/register', async (req, res) => {
+    try {
+        // Generate password by bcrypt library
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        // Create a new User
+        const newUser = new User({
+            userName: req.body.userName,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        // Save the new User and reponse
+        const user = await newUser.save();
+        return res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// LOGIN
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+        if (!validPassword) {
+            return res.status(400).json({ error: 'Wrong password' });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+module.exports = router;
